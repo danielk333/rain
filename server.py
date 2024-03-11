256224543352cead225ce02cdad284f02e83d6d0
calum <callon-2@student.ltu.se>
2024-03-11 09:06:20 +0100

    Incoming messages are cleared once they have been read (only works for valid messages

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ import time
 
 server_address = ('localhost', 10000)
 server_open = False
+encoding = 'utf-8'
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind(server_address)
@@ -13,7 +14,7 @@ print(f'Server started on {server_address[0]} with port {server_address[1]}')
 while server_open:
     print("I am a WIP server waiting for a friend")
     connection, client_address = sock.accept()
-    print("I have found a friend!")
+    print(f'I have found a friend at {client_address}!')
 
     # data = connection.recv(1)
     # data.decode('utf-8')
@@ -24,26 +25,26 @@ while server_open:
     t0 = time.time()
     dt = 0.0
     end_char = '\n'
-    code = 'utf-8'
     while dt < timeout:
         dt = time.time() - t0
 
         data = ''
         while len(data) < max_len:
             data_raw = connection.recv(1)
-            if data_raw == end_char.encode(code):
+            if data_raw == end_char.encode(encoding):
                 break
             elif len(data_raw) > 0:
-                data += data_raw.decode(code)
+                data += data_raw.decode(encoding)
             else:
                 dt = time.time() - t0
                 if dt >= timeout:
                     break
-            # print(data)
+            print(data)
 
             if data == 'echo':
                 msg = data
-                connection.sendall(msg.encode('utf-8'))
+                connection.sendall(msg.encode(encoding))
+                data = ''
             if data == 'a':
                 msg = data
-                connection.sendall(msg.encode('utf-8'))
+                connection.sendall(msg.encode(encoding))
