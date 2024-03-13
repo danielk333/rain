fff55775e7b25aba0160e6ba37e41d46b69306b9
calum <callon-2@student.ltu.se>
2024-03-13 13:40:04 +0100

    Converted from socket to pyzmq, now back to a very basic client-server interface

--- client.py
+++ client.py
@@ -1,39 +1,34 @@
 import socket
 import sys
 import time
+import zmq
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_address = ('localhost', 10000)
 encoding = 'utf-8'
 max_len = 256
 end_char = '\n'
-dt = 0.0
-timeout = 5.0
 
 def send_commands(end_char, encoding):
     command = input()
-    data = command + end_char
-    sock.sendall(data.encode(encoding))
+    socket.send_string(command, 0, encoding)
+    # data = command + end_char
+    # sock.sendall(data.encode(encoding))
 
     return command
 
-def receive_data(max_len, end_char, encoding, dt, timeout):
+def receive_data(max_len, end_char, encoding):
     data = ''
-    t0 = time.time()
     while len(data) < max_len:
         data_raw = sock.recv(1)
         if data_raw == end_char.encode(encoding):
             break
         elif len(data_raw) > 0:
             data += data_raw.decode(encoding)
-        else:
-            dt = time.time() - t0
-            if dt >= timeout:
-                break
 
     return data
 
-def interpret(data, command, connected):
+def interpret(data, command, connected, encoding):
     print(f'Server Response: {data}')
     if command == 'close':
         connected = False
@@ -41,14 +36,18 @@ def interpret(data, command, connected):
         connected = False
     return connected
 
+# Initialising the client and connecting to the server
+context = zmq.Context()
+socket = context.socket(zmq.REQ)
+socket.connect(f'tcp://{server_address[0]}:{server_address[1]}')
 print(f'Connecting to {server_address[0]} on port {server_address[1]}')
-
-connection = sock.connect(server_address)
 connected = True
 
 while connected:
-    command = send_commands(end_char, encoding)
-    reception = receive_data(max_len, end_char, encoding, dt, timeout)
-    connected = interpret(reception, command, connected)
+    # command = send_commands(end_char, encoding)
+    socket.send_string('Test', 0, True, encoding)
+    print(socket.recv_string(0, encoding))
+    # reception = receive_data(max_len, end_char, encoding)
+    # connected = interpret(reception, command, connected)
 
 sys.exit()
--- server.py
+++ server.py
@@ -1,28 +1,21 @@
 import socket
 import time
+import zmq
 
 server_address = ('localhost', 10000)
 server_open = False
 encoding = 'utf-8'
-timeout = 5.0
 max_len = 256
-t0 = time.time()
-dt = 0.0
 end_char = '\n'
 
 def receive_command(max_len, end_char, encoding, dt, timeout):
     command = ''
-    t0 = time.time()
     while len(command) < max_len:
         data_raw = connection.recv(1)
         if data_raw == end_char.encode(encoding):
             break
         elif len(data_raw) > 0:
             command += data_raw.decode(encoding)
-        else:
-            dt = time.time() - t0
-            if dt >= timeout:
-                break
 
     print(command)
     return command
@@ -48,18 +41,22 @@ def send_feedback(command, connected, server_open):
 
     return connected, server_open
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.bind(server_address)
-sock.listen()
+# Initialising and starting up the server
+context = zmq.Context()
+socket = context.socket(zmq.REP)
+socket.bind(f'tcp://{server_address[0]}:{server_address[1]}')
 server_open = True
 print(f'Server started on {server_address[0]} with port {server_address[1]}')
 
 while server_open:
-    print("I am a WIP server waiting for a friend")
-    connection, client_address = sock.accept()
-    print(f'I have found a friend at {client_address}!')
+    print("I am a WIP server ready to talk to a friend")
+    # Add something that check whether a connection has been made
     connected = True
 
     while connected == True:
-        command = receive_command(max_len, end_char, encoding, dt, timeout)
-        connected, server_open = send_feedback(command, connected, server_open)
+        # command = receive_command(max_len, end_char, encoding)
+        command = socket.recv_string(0, encoding)
+        print(command)
+        socket.send_string('Received', 0, True, encoding)
+        time.sleep(2)
+        # connected, server_open = send_feedback(command, connected, server_open)
