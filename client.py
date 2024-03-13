d9a3e3a678bfaa9e2cf45b3aa015808f395b1099
calum <callon-2@student.ltu.se>
2024-03-13 14:42:44 +0100

    A connection is now only made when a command is sent and closes once the response has been received

--- client.py
+++ client.py
@@ -1,34 +1,25 @@
-import sys
 import zmq
 
 server_address = ('localhost', 10000)
 encoding = 'utf-8'
 
-def send_commands(encoding):
+def send_command():
+    print(f'Please enter the command you would like to send to {server_address[0]}:')
     command = input()
+    socket.connect(f'tcp://{server_address[0]}:{server_address[1]}')
     socket.send_string(command, 0, True, encoding)
 
     return command
 
-def receive_data(command, connected, encoding):
+def receive_data(command):
     feedback = socket.recv_string(0, encoding)
     print(f'Server Response: {feedback}')
-    if command == 'close':
-        connected = False
-    if command == 'shutdown':
-        connected = False
 
-    return connected
+    return
 
 # Initialising the client and connecting to the server
 context = zmq.Context()
 socket = context.socket(zmq.REQ)
-socket.connect(f'tcp://{server_address[0]}:{server_address[1]}')
-print(f'Connecting to {server_address[0]} on port {server_address[1]}')
-connected = True
 
-while connected:
-    command = send_commands(encoding)
-    connected = receive_data(command, connected, encoding)
-
-sys.exit()
+command = send_command()
+receive_data(command)
--- server.py
+++ server.py
@@ -4,44 +4,34 @@ server_address = ('localhost', 10000)
 server_open = False
 encoding = 'utf-8'
 
-def receive_command(encoding):
+def receive_command():
     command = socket.recv_string(0, encoding)
     print(f'Command received: {command}')
 
     return command
 
-def send_feedback(command, connected, server_open):
+def send_feedback(command, server_open):
     if command == 'echo':
         feedback = command
-        socket.send_string(f'Received {feedback}', 0, True, encoding)
-    elif command == 'close':
-        feedback = 'Closing the connection'
-        socket.send_string(f'Received {feedback}', 0, True, encoding)
-        connected = False
+        socket.send_string(feedback, 0, True, encoding)
     elif command == 'shutdown':
-        feedback = 'Closing the connection and shutting down the server'
-        socket.send_string(f'Received {feedback}', 0, True, encoding)
-        print('Shutting down the server')
-        connected = False
+        feedback = 'Shutting down the server'
+        socket.send_string(feedback, 0, True, encoding)
+        print(feedback)
         server_open = False
     else:
         feedback = command
-        socket.send_string(f'Received {feedback}', 0, True, encoding)
+        socket.send_string(feedback, 0, True, encoding)
 
-    return connected, server_open
+    return server_open
 
 # Initialising and starting up the server
 context = zmq.Context()
 socket = context.socket(zmq.REP)
 socket.bind(f'tcp://{server_address[0]}:{server_address[1]}')
 server_open = True
-print(f'Server started on {server_address[0]} with port {server_address[1]}')
+print(f'I am a WIP server open on {server_address[0]} with port {server_address[1]} ready to talk to friends')
 
 while server_open:
-    print("I am a WIP server ready to talk to a friend")
-    # Add something that check whether a connection has been made
-    connected = True
-
-    while connected == True:
-        command = receive_command(encoding)
-        connected, server_open = send_feedback(command, connected, server_open)
+    command = receive_command()
+    server_open = send_feedback(command, server_open)
