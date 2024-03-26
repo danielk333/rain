de4557586308d04b9ac2901d99a912cc65014aef
calum <callon-2@student.ltu.se>
2024-03-26 15:39:51 +0100

    Made a keypair generation script

---
+++ keygen.py
@@ -0,0 +1,17 @@
+import shutil
+import zmq.auth
+import os
+
+home = os.path.dirname(__file__)
+dir_pub = os.path.join(home, 'public_keys')
+dir_prv = os.path.join(home, 'private_keys')
+
+def new_keypair(infra_name):
+    # Public and private keys are generated in two separate files
+    file_pub, file_prv = zmq.auth.create_certificates(home, infra_name)
+
+    # Put the keypair files into separate directories
+    shutil.move(os.path.join(home, file_pub), os.path.join(dir_pub, '.'))
+    shutil.move(os.path.join(home, file_prv), os.path.join(dir_prv, '.'))
+
+new_keypair('key_test')
