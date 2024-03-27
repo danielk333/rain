41bdc0fd85193aa783155cda10158ba27d9a12ee
calum <callon-2@student.ltu.se>
2024-03-27 16:49:32 +0100

    The private key is added to a new file, plus info generation code is grouped into functions

--- new_user.py
+++ new_user.py
@@ -1,45 +1,74 @@
 from keygen import new_keypair
 import os
 
+def key_detect(key_file):
+    with open(key_file, 'r') as in_file:
+        row_found = False
+        while not row_found:
+            line = in_file.readline()
+            if line[0:5] == 'curve':
+                row_found = True
+                line = in_file.readline()
+        pos = []
+        for iter in range(len(line)):
+            if line[iter] == '"':
+                pos.append(iter)
+        key = line[pos[0]+1: pos[1]]
+
+    return key
+
+## TODO 2: Add the time the keypair was generated
+def generate_static(name, key):
+    with open(f'{name}.info', 'w') as static:
+        static.write(name.upper())
+        static.write('\n\n')
+        static.write('Contact Name:\n')
+        static.write('Contact Address:\n')
+        static.write('Contact Coordinates:\n\n')
+
+        static.write('Type: ZeroMQ CURVE Public Certificate\n')
+        static.write(f'Generated: \n')
+        static.write('curve\n')
+        static.write(f'    public-key = {key_pub}\n')
+
+        static.write('\n' + '-------------------\n\n')
+
+    return
+
+## TODO 2
+def generate_secret(name, key_pub, key_prv):
+    with open(f'{name}.private', 'w') as secret:
+        secret.write(name.upper() + '\n\n')
+        secret.write('Type: ZeroMQ CURVE **Secret** Certificate\n\n')
+        secret.write(f'Generated: \n')
+        secret.write('curve\n')
+        secret.write(f'    public-key = {key_pub}\n')
+        secret.write(f'    secret-key = {key_prv}\n')
+
+    return
+
 home = os.path.dirname(__file__)
 dir_pub = os.path.join(home, 'public_keys')
 dir_prv = os.path.join(home, 'private_keys')
 
 print('Please enter the name of your instrument:')
-infra_name = input()
+infra_name = input().lower()
 
 # Generate a key pair for the new instrument
-## TODO: Check there is no existing instrument with this name, and no existing keypair
+## TODO 1: Check there is no existing instrument with this name, and no existing keypair
 file_pub, file_prv = new_keypair(infra_name)
-print(file_pub)
 
-# Record public key
-with open(file_pub, 'r') as public:
-    row_found = False
-    while not row_found:
-        line = public.readline()
-        if line[0:5] == 'curve':
-            row_found = True
-            line = public.readline()
-    pos = []
-    for iter in range(len(line)):
-        if line[iter] == '"':
-            pos.append(iter)
-    key_pub = line[pos[0]+1: pos[1]]
+# Extract public and private keys from the automatically generated files
+key_pub = key_detect(file_pub)
+key_prv = key_detect(file_prv)
+
+# Create a new file for the private key
+generate_secret(infra_name, key_pub, key_prv)
+
+# TODO 3: Delete the automatically generated files
 
 
 # Create an info file and write in it the static information
-with open(f'{infra_name}.info', 'w') as static:
-    static.write(infra_name.upper())
-    static.write('\n\n')
-    static.write('Contact Name:\n')
-    static.write('Contact Address:\n')
-    static.write('Contact Coordinates:\n\n')
-
-    static.write(f'Public Key: {key_pub}\n')
-    static.write(f'Generated: \n')
-    static.write('Type: ZeroMQ CURVE Public Certificate\n')
-
-    static.write('\n' + '-------------------\n\n')
+generate_static(infra_name, key_pub)
 
 print(f'Thank you for registering {infra_name} to RAIN!')
