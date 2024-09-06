1b6bf04f6c5eefe6ec2057607b787e78884ae250
danielk <daniel.kastinen@irf.se>
2024-09-06 10:49:46 +0200

    add version in main context

--- src/rain/__init__.py
+++ src/rain/__init__.py
@@ -2,3 +2,5 @@ from .cli import trigger_cli, register_cli, client_cli, server_cli
 from .plugins import PLUGINS, register_publish, register_response, register_trigger
 
 from . import get_api
+
+from .version import __version__
