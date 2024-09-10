6f62df7d07d712b18e23d85246dc064b5419e19d
danielk <daniel.kastinen@irf.se>
2024-09-10 15:36:55 +0200

    expose plugin load

--- src/rain/__init__.py
+++ src/rain/__init__.py
@@ -1,5 +1,5 @@
 from .cli import trigger_cli, register_cli, client_cli, server_cli
-from .plugins import PLUGINS, register_publish, register_response, register_trigger
+from .plugins import PLUGINS, register_publish, register_response, register_trigger, load_plugins
 
 from . import get_api
 
